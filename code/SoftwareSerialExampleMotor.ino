4a6d8af2df6da485e2c35517be8a79a068720486
Stan-en-Tim <99021939+Stan-en-Tim@users.noreply.github.com>
2022-02-11 10:56:07 +0100

    Update SoftwareSerialExampleMotor.ino

--- code/SoftwareSerialExampleMotor.ino
+++ code/SoftwareSerialExampleMotor.ino
@@ -1,34 +1,16 @@
 /*
-  Software serial multple serial test
+ Dit is een aangepaste versie van het sofwareserial exapmle.
+  
+   created back in the mists of time
+   modified 25 May 2012
+   by Tom Igoe
+   based on Mikal Hart's example
 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
+ Ontvang data van software serial (Motor), stuur door naar hardware serial (USB, Serial Monitor).
+ Dit gebruiken we om het signaal dat tussen de motor en de gashendel wordt gestuurd af te luisteren.
+*/
 #include <SoftwareSerial.h>
 
-int packet[] = {0xAC,0x20,0x41,0,6,1,0,0,0,0,0,0,0,0,0,0,0,0,9,0xAA,0,0,0,0,0,0,0,0,0,0x13,0x97,0xAD,0xFF};
-int packet2[] = {0xAC,0x20,0x41,0,6,1,0};
 int len = 33;
 SoftwareSerial mySerial(2, 3); // RX, TX
 
@@ -41,12 +23,8 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-
-  Serial.println("Goodnight moon!");
-
   // set the data rate for the SoftwareSerial port
   mySerial.begin(19200);
-  //mySerial.println("Hello, world?");
   delay(1000);
   digitalWrite(6, HIGH);
   digitalWrite(5, HIGH);
